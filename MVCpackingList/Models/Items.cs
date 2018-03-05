b6c8788857f90ef0cb4f38bef961f5a2e09faafd
JuliePretzlaff <juliapretzlaff@gmail.com>
2018-03-05 14:41:17 -0500

    controller created, need to style

---
+++ MVCpackingList/Models/Items.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCpackingList.Models
+{
+    public class Items
+    {
+
+        [Key]
+        public int ItemID { get; set; }
+
+        public string ItemName { get; set; }
+
+        public int Quantity { get; set; }
+
+        public bool IsCarryOn { get; set; }
+    }
+}
